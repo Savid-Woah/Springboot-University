9c5034ffa7beebec0d762f0c3d2e5e01dfde254a
Savid-Woah <savidoficial09@gmail.com>
2024-06-16 18:01:53 -0500

    - Dependencies added - EnrollmentRequest dir change

---
+++ src/main/java/springboot_university/course/request/EnrollRequest.java
@@ -0,0 +1,25 @@
+package springboot_university.course.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class EnrollRequest {
+
+    @NotNull(message = "Field required")
+    private UUID courseId;
+
+    @NotNull(message = "Field required")
+    private UUID studentId;
+}
